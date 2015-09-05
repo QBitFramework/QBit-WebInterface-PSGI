7179e1fb67abe9962d7e8b57699e2624f8e25a67
Igor Sverdlov <igor-sverdlov@mail.ru>
2015-09-05 20:13:35 +0300

    QBit::WebInterface::PSGI

---
+++ lib/QBit/WebInterface/PSGI/Request.pm
@@ -0,0 +1,36 @@
+package QBit::WebInterface::PSGI::Request;
+
+use qbit;
+
+use base qw(QBit::WebInterface::Request);
+
+sub http_header {
+    my ($self, $name) = @_;
+
+    $name =~ s/-/_/g;
+    my $value = $self->{'ENV'}{'HTTP_' . uc($name)};
+
+    return defined($value) ? $value : '';
+}
+
+sub method {shift->{'ENV'}{'REQUEST_METHOD'}}
+
+sub uri {shift->{'ENV'}{'REQUEST_URI'}}
+
+sub scheme {shift->{'ENV'}{'psgi.url_scheme'}}
+
+sub server_name {shift->{'ENV'}{'SERVER_NAME'}}
+
+sub server_port {shift->{'ENV'}{'SERVER_PORT'}}
+
+sub remote_addr {shift->{'ENV'}{'REMOTE_ADDR'}}
+
+sub query_string {shift->{'ENV'}{'QUERY_STRING'}}
+
+sub _read_from_stdin {
+    my ($self, $buffer_ref, $size) = @_;
+
+    return read($self->{'ENV'}{'psgi.input'}, $$buffer_ref, $size);
+}
+
+TRUE;
